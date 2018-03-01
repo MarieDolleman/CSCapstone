5f93eac60d0e729f5564cce99346a9e6698e511d
Marie Dolleman <marie.dolleman@drake.edu>
2018-03-01 12:48:28 -0600

    Finished tables working, but not progress tables

--- iditarod.py
+++ iditarod.py
@@ -5,10 +5,12 @@ import scrape_data
 def main():
     log_number = 583
     # Current check point not previous checkpoint
-    wanted_keys = ['Pos', 'Musher', 'Checkpoint', 'Dogs', 'Total Race Time']
-    extra_keys = ['Previous', 'In', 'Out', 'LayoverCompleted']
-    log = scrape_data.log_data(log_number, wanted_keys, extra_keys)
-    race_stats = race.Race({'fake': 'dict'})
+    progress_keys = ['Pos', 'Musher', 'Bib', 'Checkpoint', 'Dogs', 'rookie']
+    finished_keys = ['Pos', 'Musher', 'Bib', 'Checkpoint', 'Total Race Time', 'Dogs In', 'rookie']
+    extra_keys = ['Previous', 'Out', 'In', 'LayoverCompleted', 'Status']
+    # extra keys originally passed to organize_data
+    log = scrape_data.log_data(log_number, finished_keys, progress_keys, extra_keys)
+    race_stats = race.Race(log[0])
 
     def reached_Nome():
         return race_stats.in_Nome()
--- scrape_data.py
+++ scrape_data.py
@@ -1,64 +1,81 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+def numeric_convert(ele):
+    try:
+        return int(ele)
+    except ValueError:
+        return None
+
 def data_collect(log_number):
     url = 'https://iditarod.com/race/2017/logs/' + str(log_number) + '/'
     page = urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
 
     # table of data
-    table = soup.find_all('table', attrs={'class':'current-standings'})
+    raw = soup.find_all('table', attrs={'class':'current-standings'})
+    return raw
 
-    keys = table[0].find_all('th')
+
+def table_clean(raw):
+    keys = raw[0].find_all('th')
     finished_keys = []
-    for i, e in enumerate(keys):
-        finished_keys.append(e.text.strip())
-    finished = table[0].text.strip()
+    for col in keys:
+        finished_keys.append(col.text.strip())
+    finished = raw[0].text.strip()
     finished = finished.split('\n')
 
-    if len(table) > 1:
-        keys = table[1].find_all('th')
+    if len(raw) > 1:
+        keys = raw[1].find_all('th')
         racing_keys = []
-        for i, e in enumerate(keys):
-            racing_keys[i] = e.text.strip()
-        racing = table[1].text.strip()
-        racing = racing.spilt('\n')
-        return [racing_keys, racing, finished_keys, finished] 
+        for col in keys:
+            racing_keys.append(col.text.strip())
+        racing = raw[1].text.strip()
+        racing = racing.split('\n')
+        return [finished_keys, finished, racing_keys, racing] 
 
     return [finished_keys, finished]
 
-def organize_data(table, keys, wanted_keys, extra_keys):
+def organize_data(keys, table, wanted_keys, extra_keys):
     kindex = 0
     musher_list = []
-    
-    if not set(wanted_keys).issubset(table):
-        raise KeyError('The data ingest process has broken. The log keys have been changed')
 
     mushers = {}
     for ele in table:
-        if ele == '':
-            continue
-        if (ele in keys) or (ele not in wanted_keys):
-            continue
-        if '(r)' in ele:
-            ele.strip('(r)')
-            mushers['rookie_status'] = True
+        while keys[kindex] in extra_keys:
+            kindex += 1
+            if kindex <= len(keys):
+                kindex = 0
         print(keys[kindex], ele)
-        mushers[keys[kindex]] = ele
-        kindex += 1
-        if kindex >= len(keys):
+        if ele == '':
             kindex = 0
-            for key in mushers:
-                if key not in wanted_keys:
-                    del mushers[key]
-            musher_list.append(mushers)
             mushers = {}
+            continue
+        elif (ele in keys):
+            continue
+        
+        elif (keys[kindex] in wanted_keys) and (keys[kindex] not in list(mushers.keys())):
+            if '(r)' in ele:
+                ele.strip('(r)')
+                mushers['rookie_status'] = True
+            else:
+                mushers['rookie_status'] = False
+            mushers[keys[kindex]] = ele
+            kindex += 1
+            if len(mushers) == len(wanted_keys):
+                kindex = 0
+                musher_list.append(mushers)
+        else:
+            kindex += 1
     return musher_list
 
-def log_data(log_number, wanted_keys, extra_keys):
-    tables = data_collect(log_number)
-    musher_list = organize_data(tables[0], tables[1], wanted_keys, extra_keys)
+def log_data(log_number, finished_keys, progress_keys, extra_keys):
+    raw_data = data_collect(log_number)
+    # returns key then corresponding table, then possibly second set of keys and table
+    tables = table_clean(raw_data)
+    musher_list = organize_data(tables[0], tables[1], finished_keys, extra_keys)
     if len(tables) > 2:
-        musher_list.append(organize_data(tables[2], tables[3], wanted_keys, extra_keys))
+        musher_list.append(organize_data(tables[2], tables[3], progress_keys, extra_keys))
+    print(musher_list)
     return musher_list
 
